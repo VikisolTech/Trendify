0cbe1f1c157058a0171cc31ea57ff5c0d566638a
Vimal Pandey <vimalpandey@Vimals-Laptop.local>
2024-05-06 13:32:20 +0530

    mui-error and code review

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider as ReactReduxProvider } from "react-redux";
+import { ThemeProvider as MuiProvider, createTheme } from "@mui/material";
 import store from "./main-test/redux/Store";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const MuiTheme = createTheme();
 root.render(
   <React.StrictMode>
     <ReactReduxProvider store={store}>
       <ChakraProvider>
-        <App />
+        <MuiProvider theme={MuiTheme}>
+          <App />
+        </MuiProvider>
       </ChakraProvider>
-      
     </ReactReduxProvider>
   </React.StrictMode>
 );
